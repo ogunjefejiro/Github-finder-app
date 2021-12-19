cd43c937df3778c332697b906e66e5411baf1979
Fejiro Ogunje <ogunjefejiro@gmail.com>
2021-12-19 23:26:49 +0100

    sixth commit

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -10,7 +10,7 @@ const UserResults = () => {
 
     if(!loading) {
         return (
-            <div className="grid grid-cols-2 gap-8 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2">
+            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2">
                 {users.map((user) => (<UserItem key={user.id} user={user}/>))}
             </div>
         ) 
