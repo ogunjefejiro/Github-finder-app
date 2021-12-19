5ef21c2ad152c46dd506785bb724aae947e268c9
Fejiro Ogunje <ogunjefejiro@gmail.com>
2021-12-19 20:47:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import User from "./pages/User";
+import { GithubProvider } from "./context/github/GithubContext";
+import {AlertProvider} from "./context/alert/AlertContext"
+import Alert from "./components/layout/Alert";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubProvider>
+      <AlertProvider>
+        <BrowserRouter>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            
+            <main className="container px-3 pb-12 mx-auto">
+              <Alert/>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+            
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </AlertProvider>
+    </GithubProvider>
   );
 }
 
+
 export default App;
+
