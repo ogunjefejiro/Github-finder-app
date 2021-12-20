dc80b9e38f76a55bfd965385059f864987da9879
Fejiro Ogunje <ogunjefejiro@gmail.com>
2021-12-20 04:31:07 +0100

    final commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
         <div>
             <h1 className="text-6xl mb-4">Github Finder App</h1>
             <p>This app can be used to search for different Github users, view their past works and repositories, check their following and followers etc.</p>
-            <p>This prohect is part of an Udemy course i'm currently taking titled "React Front to Back 2022" by Brad Traversry.</p>
+            <p>This project is part of an Udemy course i'm currently taking titled "React Front to Back 2022" by Brad Traversry.</p>
         </div>
     )
 }
